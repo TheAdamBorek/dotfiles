fe41485f826e3cec6f4d384d2c309d52d0725546
Adam Borek <adam@attio.com>
2024-08-25 11:03:27 +0200

    Small changes in few plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.cmd 'language en_US'
 require 'vaquel'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/vaquel/plugins/quickscope.lua
+++ lua/vaquel/plugins/quickscope.lua
@@ -2,10 +2,10 @@ return {
   'unblevable/quick-scope',
   config = function()
     -- Optional: Customize quick-scope settings here
-    vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+    -- vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
     vim.cmd [[
                 highlight QuickScopePrimary guifg='#fabd2f' gui=bold ctermfg=214 cterm=bold
-                highlight QuickScopeSecondary guifg='#fac72f' gui=bold ctermfg=108 cterm=bold
+                highlight QuickScopeSecondary guifg='#ff7a93' gui=bold ctermfg=108 cterm=bold
             ]]
   end,
 }
