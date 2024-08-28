a62867b98dd9018d17c4621a4a648a792d4e0982
Adam Borek <adam@attio.com>
2024-08-28 21:43:53 +0200

    turn on auto auto-session

--- lua/vaquel/plugins/alpha.lua
+++ lua/vaquel/plugins/alpha.lua
@@ -23,7 +23,6 @@ return {
       dashboard.button('SPC ne', '  > Toggle file explorer', '<cmd>NvimTreeToggle<CR>'),
       dashboard.button('SPC sf', '󰱼 > Find File', '<cmd>Telescope find_files<CR>'),
       dashboard.button('SPC ss', '  > Find Word', '<cmd>Telescope live_grep<CR>'),
-      dashboard.button('SPC wr', '󰁯  > Restore Session For Current Directory', '<cmd>SessionRestore<CR>'),
       dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'),
     }
 
--- lua/vaquel/plugins/auto-sessions.lua
+++ lua/vaquel/plugins/auto-sessions.lua
@@ -3,8 +3,4 @@ return {
   opts = {
     auto_session_suppress_dirs = { '~/', '~/Developer/', '~/Downloads', '~/Documents', '~/Desktop/' },
   },
-  keys = {
-    { '<leader>wr', '<cmd>SessionRestore<CR>', { desc = 'Restore session for cwd' } },
-    { '<leader>ws', '<cmd>SessionSave<CR>', { desc = 'Save session for auto session root dir' } },
-  },
 }
