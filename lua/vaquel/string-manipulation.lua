c3f2eee56aa28517c5fc7d7d752121b70aa54368
Adam <adambor3k@gmail.com>
2024-09-01 10:04:29 +0200

    adds CamelCase to snake_case conversion

--- lua/vaquel/string-manipulation.lua
+++ lua/vaquel/string-manipulation.lua
@@ -1,2 +1,5 @@
 -- Converts a CamelCase to kebab-case
 vim.keymap.set('v', 'gk', '<cmd>s/\\([a-zA-Z]\\)\\([A-Z]\\)/\\1-\\l\\2/g<CR><cmd>s/^\\([A-Z]\\)/\\l\\1/<CR><Esc>', { desc = 'Convert camelCase to kebab-case' })
+
+-- Converts a CamelCase to snake_case
+vim.keymap.set('v', 'gs', '<cmd>s/\\([a-z]\\)\\([A-Z]\\)/\\1_\\l\\2/g<CR><cmd>s/\\([A-Z]\\)/\\l&/g<CR><Esc>', { desc = 'Convert camelCase to snake_case' })
