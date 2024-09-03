a0becbef89f5ee662b78ef1401fd462a85c1b05a
Adam <adambor3k@gmail.com>
2024-09-03 09:37:58 +0200

    adds terminal command

--- lua/vaquel/plugins/vim-tmux-navigator.lua
+++ lua/vaquel/plugins/vim-tmux-navigator.lua
@@ -1,6 +1,21 @@
+local open_terminal_window = function()
+  -- Check the number of panes in the current tmux window
+  local pane_count = tonumber(vim.fn.system 'tmux list-panes | wc -l')
+  local need_to_create_pane = pane_count < 2
+  if need_to_create_pane then
+    -- Create a new horizontal split if there's only one pane (horizontal means split below)
+    os.execute 'tmux split-window -v -l 33%'
+  else
+    os.execute 'tmux resize-pane -Z'
+    os.execute 'tmux select-pane -D'
+  end
+end
+
 return {
   'christoomey/vim-tmux-navigator',
   config = function()
     vim.g.tmux_navigator_disable_when_zoomed = 1
+
+    vim.keymap.set('n', '<C-w>t', open_terminal_window, { desc = 'Open [t]erminal window' })
   end,
 }
