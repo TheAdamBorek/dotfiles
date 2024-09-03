6541a873a683a18ef9255e7c1f253fe67db93cd3
Adam Borek <adam@attio.com>
2024-09-03 18:26:12 +0200

    fixes terminal navigation

--- lua/vaquel/plugins/vim-tmux-navigator.lua
+++ lua/vaquel/plugins/vim-tmux-navigator.lua
@@ -1,16 +1,18 @@
 local open_terminal_window = function()
+  print 'hello'
   local command = [[
-tmux list-panes -F "#{pane_active} #{pane_top}" | awk -v bottom=$(tmux display-message -p "#{pane_bottom}") '
-$1 == "1" {current_bottom=bottom}
-$2 > current_bottom {print "Pane below exists"; found=1; exit}
-END {if (!found) print "No pane below"}'
-]]
-  local has_pane_below = vim.fn.system(command) == 'Pane below exists\n'
+  tmux list-panes -F "#{pane_active} #{pane_top}" | awk '
+  $1 == "1" {current_top = $2}
+  $2 > current_top {print "Pane below exists"; found=1; exit 0}
+  END {if (!found) print "No pane below"; exit 1}'
+  ]]
+  local result = vim.fn.system(command)
+  local has_pane_below = result == 'Pane below exists\n'
   if not has_pane_below then
-    os.execute 'tmux split-window -v -l 33%'
+    vim.fn.system 'tmux split-window -v -l 33%'
   else
-    os.execute 'tmux resize-pane -Z'
-    os.execute 'tmux select-pane -D'
+    vim.fn.system 'tmux resize-pane -Z'
+    vim.fn.system 'tmux select-pane -D'
   end
 end
 
@@ -18,7 +20,7 @@ return {
   'christoomey/vim-tmux-navigator',
   config = function()
     vim.g.tmux_navigator_disable_when_zoomed = 1
-
+    vim.g.tmux_navigator_disable_netrw_workaround = 1
     vim.keymap.set('n', '<C-w>t', open_terminal_window, { desc = 'Open [t]erminal window' })
   end,
 }
