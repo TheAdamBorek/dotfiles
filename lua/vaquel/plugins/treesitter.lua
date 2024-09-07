02390bbf2a0bce8f1f04a6e372314b36fa6c13ae
Adam <adambor3k@gmail.com>
2024-09-07 15:04:16 +0200

    Fixes treesitter injection higlighting!

--- lua/vaquel/plugins/treesitter.lua
+++ lua/vaquel/plugins/treesitter.lua
@@ -25,6 +25,7 @@ return { -- Highlight, edit, and navigate code
       'json',
       'gitignore',
       'css',
+      'styled',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
