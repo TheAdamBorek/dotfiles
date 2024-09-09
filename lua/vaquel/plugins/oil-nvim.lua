02b61ae269378e313f796d7746b90ff014fe4d1a
Adam Borek <adam@attio.com>
2024-09-09 12:56:54 +0200

    adds oil.nvim settings

--- lua/vaquel/plugins/oil-nvim.lua
+++ lua/vaquel/plugins/oil-nvim.lua
@@ -1,8 +1,23 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('oil').setup()
+  opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name)
+        return name == '.git' or name == '.DS_Store'
+      end,
+    },
+    win_options = {
+      wrap = true,
+    },
+  },
+  config = function(_, opts)
+    require('oil').setup(opts)
     vim.keymap.set('n', '-', '<cmd>Oil<CR>', { noremap = true })
   end,
 }
