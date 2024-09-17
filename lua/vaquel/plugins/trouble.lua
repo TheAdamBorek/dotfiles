faf6d9dc20603a1dffc197512ed2d1d4f7613a1b
Adam <adambor3k@gmail.com>
2024-09-17 22:11:10 +0200

    disable trouble

--- lua/vaquel/plugins/trouble.lua
+++ lua/vaquel/plugins/trouble.lua
@@ -1,14 +1,17 @@
 return {
   'folke/trouble.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
+  enabled = false,
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim', 'folke/which-key.nvim' },
   opts = {
     focus = true,
   },
   cmd = 'Trouble',
+  init = function()
+    require('which-key').add { '<leader>T', group = '[T]rouble' }
+  end,
   keys = {
     { '<leader>Tw', '<cmd>Trouble diagnostics toggle<CR>', desc = 'Open trouble workspace diagnostics' },
     { '<leader>Td', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = 'Open trouble document diagnostics' },
-    -- { '<leader>Tq', '<cmd>Trouble quickfix toggle<CR>', desc = 'Open trouble quickfix list' },
     { '<leader>Tl', '<cmd>Trouble loclist toggle<CR>', desc = 'Open trouble location list' },
   },
 }
