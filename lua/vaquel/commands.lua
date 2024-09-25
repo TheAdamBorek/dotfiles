50fe3ee7f5d22a9496a3deebbda2e66fafc03299
Adam Borek <adam@attio.com>
2024-09-25 12:07:05 +0200

    adds a relative path command

--- lua/vaquel/commands.lua
+++ lua/vaquel/commands.lua
@@ -1,1 +1,8 @@
 vim.api.nvim_create_user_command('VSCodeOpenIn', '!code . && code -g %', {})
+
+-- Copy relative path to clipboard
+vim.api.nvim_create_user_command('PathRelativeCopy', function()
+  local path = vim.fn.fnamemodify(vim.fn.expand '%', ':.')
+  vim.fn.setreg('+', path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
