eddcc7f941ee4c6a40886d3ffff004c5f4f92eb3
Adam Borek <adam@attio.com>
2024-09-26 14:03:06 +0200

    fixes command to be pipeable

--- lua/vaquel/commands.lua
+++ lua/vaquel/commands.lua
@@ -10,4 +10,4 @@ end, {})
 -- Regex to edit icons
 vim.api.nvim_create_user_command('AttioIconsRegex', function()
   vim.cmd [[silent! %s/stroke="#\?\w*"/stroke="var(--color)"/g | silent! %s/fill="[^none|white]#\?\w*"/fill="var(--color)"/g]]
-end, {})
+end, { bar = true })
