acfae2013085704362d6ef602d8b22c2c07a5311
Adam Borek <adam@attio.com>
2024-09-26 11:58:01 +0200

    increase n_lines in mini-ai

--- lua/vaquel/plugins/mini-ai.lua
+++ lua/vaquel/plugins/mini-ai.lua
@@ -1,5 +1,6 @@
 return {
   'echasnovski/mini.ai',
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
   config = function()
     local mini_ai = require 'mini.ai'
     local spec_treesitter = mini_ai.gen_spec.treesitter
@@ -8,6 +9,7 @@ return {
       custom_textobjects = {
         f = spec_treesitter { a = '@function.outer', i = '@function.inner' },
       },
+      n_lines = 100,
     }
   end,
 }
