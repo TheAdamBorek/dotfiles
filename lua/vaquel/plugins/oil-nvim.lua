32c1441f576e6fc9d2d1fedbff1e36477d34bbbd
Adam Borek <adam@attio.com>
2024-09-26 08:43:52 +0200

    setup keybinds for oil

--- lua/vaquel/plugins/oil-nvim.lua
+++ lua/vaquel/plugins/oil-nvim.lua
@@ -12,12 +12,33 @@ return {
         return name == '.git' or name == '.DS_Store'
       end,
     },
+    lsp_file_methods = {
+      -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+      -- Set to "unmodified" to only save unmodified buffers
+      autosave_changes = 'unmodified',
+    },
+    watch_for_changes = true,
     win_options = {
       wrap = true,
     },
     keymaps = {
       ['<C-q>'] = 'actions.send_to_qflist',
+      ['g?'] = 'actions.show_help',
+      ['<CR>'] = 'actions.select',
+      ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+      ['<C-s>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+      ['<C-p>'] = 'actions.preview',
+      ['<C-c>'] = 'actions.close',
+      ['<C-r>'] = 'actions.refresh',
+      ['-'] = 'actions.parent',
+      ['_'] = 'actions.open_cwd',
+      ['`'] = 'actions.cd',
+      ['gs'] = 'actions.change_sort',
+      ['gx'] = 'actions.open_external',
+      ['g.'] = 'actions.toggle_hidden',
+      ['g\\'] = 'actions.toggle_trash',
     },
+    use_default_keymaps = false,
   },
   config = function(_, opts)
     require('oil').setup(opts)
