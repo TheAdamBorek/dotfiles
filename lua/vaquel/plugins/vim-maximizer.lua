bd2f20242f1f2bcdf6767f3b953d13a374402057
Adam Borek <adam@attio.com>
2024-09-26 14:38:58 +0200

    setup maximizer keybind

--- lua/vaquel/plugins/vim-maximizer.lua
+++ lua/vaquel/plugins/vim-maximizer.lua
@@ -1,6 +1,6 @@
 return {
   'szw/vim-maximizer',
   keys = {
-    { '<C-w><C-m>', '<cmd>MaximizerToggle<CR>', desc = 'Maximize/minimize a split' },
+    { '<C-w><CR>', '<cmd>MaximizerToggle<CR>', desc = 'Maximize/minimize a split' },
   },
 }
