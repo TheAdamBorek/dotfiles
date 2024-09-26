1cd8be86f8bb5bdf2995ec45a9754f15447f0b2d
Adam Borek <adam@attio.com>
2024-09-26 08:36:43 +0200

    adds icons regex

--- lua/vaquel/commands.lua
+++ lua/vaquel/commands.lua
@@ -6,3 +6,8 @@ vim.api.nvim_create_user_command('PathRelativeCopy', function()
   vim.fn.setreg('+', path)
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
+
+-- Regex to edit icons
+vim.api.nvim_create_user_command('AttioIconsRegex', function()
+  vim.cmd [[silent! %s/stroke="#\?\w*"/stroke="var(--color)"/g | silent! %s/fill="[^none|white]#\?\w*"/fill="var(--color)"/g]]
+end, {})
