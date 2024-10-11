9dca60c6ac057ad72d2d37e5a01bbf2c12cdee18
Adam Borek <adam@attio.com>
2024-10-11 15:38:39 +0200

    fixes eslint autocmd

--- lua/vaquel/shared/js-ftplugin.lua
+++ lua/vaquel/shared/js-ftplugin.lua
@@ -6,18 +6,17 @@ local function setup_eslint_on_save()
 
   local autogroup_name = require('vaquel.shared.lint-autogroup').lint_autogroup_name
   local on_safe_autogroup = vim.api.nvim_create_augroup(autogroup_name, { clear = true })
-  vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
     group = on_safe_autogroup,
     callback = function()
       vim.cmd [[
         silent! EslintFixAll
       ]]
-      linter.try_lint()
     end,
   })
 
   local report_only_autogroup = vim.api.nvim_create_augroup('lint_readonly', { clear = true })
-  vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave' }, {
+  vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'BufWritePost' }, {
     group = report_only_autogroup,
     callback = function()
       linter.try_lint()
