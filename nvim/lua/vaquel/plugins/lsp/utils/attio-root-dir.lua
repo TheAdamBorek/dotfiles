1b3ccbc6ca81cfacb49d70b65d75a39abb205a34
Adam Borek <adam@attio.com>
2024-10-30 09:18:06 +0100

    Update plugins

--- nvim/lua/vaquel/plugins/lsp/utils/attio-root-dir.lua
+++ nvim/lua/vaquel/plugins/lsp/utils/attio-root-dir.lua
@@ -5,7 +5,7 @@ M.attio_root_dir = function(lsp_name)
   --- @param filepath string
   --- @return string
   return function(filepath)
-    local default_config = require('lspconfig.server_configurations.' .. lsp_name).default_config
+    local default_config = require('lspconfig.configs.' .. lsp_name).default_config
     local utils = require 'lspconfig.util'
     if string.match(filepath, 'job/attio/') then
       return utils.root_pattern '.git'(filepath)
