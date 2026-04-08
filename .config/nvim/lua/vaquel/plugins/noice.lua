c2c03b2ada8cbed46976a1bdb4f5a11f35b3ba3a
Adam Borek <adam@attio.com>
2026-04-08 22:55:29 +0200

    use live-rename instead of inc-rename

--- .config/nvim/lua/vaquel/plugins/noice.lua
+++ .config/nvim/lua/vaquel/plugins/noice.lua
@@ -17,7 +17,7 @@ return {
       command_palette = true,
       long_message_to_split = true,
       lsp_doc_border = false,
-      inc_rename = true,
+      inc_rename = false,
     },
   },
 }
---
+++ .config/nvim/lua/vaquel/plugins/rename.lua
@@ -0,0 +1,5 @@
+return {
+  'saecki/live-rename.nvim',
+  event = 'LspAttach',
+  opts = {},
+}
